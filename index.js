f451129838829339cd4eff679cb5105f61363e96
Bhumika Reddy <reddybhumi1999@gmail.com>
2025-07-12 14:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Success");
+})
+
 app.post("/send-push", async (req, res) => {
   const { tokens, title, body, data } = req.body;
 
